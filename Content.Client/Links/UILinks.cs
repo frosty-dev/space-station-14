e8e81bba3a3d70b1512740116e865fd9c7611589
Valtos <valtos@spaces.ru>
2022-10-31 20:20:14 +0700

    если я это поменяю, ничего страшного не случится, да?

--- Content.Client/Links/UILinks.cs
+++ Content.Client/Links/UILinks.cs
@@ -2,12 +2,12 @@
 {
     public static class UILinks
     {
-        public const string GitHub = "https://github.com/space-syndicate/space-station-14/";
-        public const string Patreon = "https://boosty.to/corvax";
+        public const string GitHub = "https://github.com/frosty-dev/space-station-14/";
+        public const string Patreon = "https://boosty.to/whitedream";
 
-        public const string Discord = "https://discord.com/invite/F9bNgSYfX5";
-        public const string BugReport = "https://github.com/space-syndicate/space-station-14/issues/new/choose";
-        public const string Website = "https://station14.ru";
-        public const string Wiki = "https://wiki.station14.ru";
+        public const string Discord = "https://discord.gg/2WAsvv5B5v";
+        public const string BugReport = "https://github.com/frosty-dev/space-station-14/issues/new/choose";
+        public const string Website = "https://station13.ru";
+        public const string Wiki = "https://wiki.station13.ru";
     }
 }
